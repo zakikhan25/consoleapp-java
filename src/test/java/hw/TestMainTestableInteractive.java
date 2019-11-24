bad00a417d6e8c65eab1b11d5a329b9db7414058
Konstantin Läufer <klaeufer@gmail.com>
2019-11-23 21:46:41 -0600

    rearranged interaction test code for clarity
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/test/java/hw/TestMainTestableInteractive.java
+++ src/test/java/hw/TestMainTestableInteractive.java
@@ -6,22 +6,47 @@ import java.util.*;
 
 import static org.junit.Assert.*;
 
+public class TestMainTestableInteractive {
+
+  @Test
+  public void testInteractiveBehavior() {
+    // the test input
+    final Iterator<String> input = Arrays.asList("asdf", "qwer", "oiui", "zxcv").iterator();
+    // the expected interaction trace
+    final List<TraceEvent> expectedTrace = new LinkedList<>();
+    expectedTrace.add(new InputEvent("asdf"));
+    expectedTrace.add(new OutputEvent("asdf"));
+    expectedTrace.add(new InputEvent("qwer"));
+    expectedTrace.add(new OutputEvent("asdf", "qwer"));
+    expectedTrace.add(new InputEvent("oiui"));
+    expectedTrace.add(new OutputEvent("asdf", "qwer", "oiui"));
+    expectedTrace.add(new InputEvent("zxcv"));
+    expectedTrace.add(new OutputEvent("qwer", "oiui", "zxcv"));
+    // create and exercise the SUT
+    final WindowMaker sut = new WindowMaker(3);
+    final List<TraceEvent> actualTrace = new Tracing(sut).run(input);
+    // make sure the expected and actual traces match
+    assertEquals(expectedTrace, actualTrace);
+  }
+}
+
 // A mini-framework for trace-based testing of interactive behavior (WIP)
 
-interface TraceEvent<Input, Result> {}
+interface TraceEvent {}
 
-class InputEvent<Input, Result> implements TraceEvent<Input, Result> {
-  private Input value;
+class InputEvent implements TraceEvent {
+  private final String value;
 
-  public InputEvent(final Input value) {
+  public InputEvent(final String value) {
     this.value = value;
   }
 
   @Override
   public boolean equals(final Object that) {
-    return that != null
-        && (that instanceof InputEvent)
-        && this.value.equals(((InputEvent) that).value);
+    return that == this
+        || that != null
+            && (that instanceof InputEvent)
+            && this.value.equals(((InputEvent) that).value);
   }
 
   @Override
@@ -30,18 +55,19 @@ class InputEvent<Input, Result> implements TraceEvent<Input, Result> {
   }
 }
 
-class OutputEvent<Input, Result> implements TraceEvent<Input, Result> {
-  private Result value;
+class OutputEvent implements TraceEvent {
+  private final List<String> value;
 
-  public OutputEvent(final Result value) {
-    this.value = value;
+  public OutputEvent(final String... values) {
+    this.value = Arrays.asList(values);
   }
 
   @Override
   public boolean equals(final Object that) {
-    return that != null
-        && (that instanceof OutputEvent)
-        && this.value.equals(((OutputEvent) that).value);
+    return that == this
+        || that != null
+            && (that instanceof OutputEvent)
+            && this.value.equals(((OutputEvent) that).value);
   }
 
   @Override
@@ -50,48 +76,43 @@ class OutputEvent<Input, Result> implements TraceEvent<Input, Result> {
   }
 }
 
-public class TestMainTestableInteractive {
+class Tracing {
 
-  @Test
-  public void testInteractiveBehavior() {
-    final WindowMaker sut = new WindowMaker(3);
-    final Iterator<String> input = Arrays.asList("asdf", "qwer", "oiui", "zxcv").iterator();
-    // the actual event trace instance
-    final List<TraceEvent<String, List<String>>> trace = new LinkedList<>();
-    // instrument the iterator so that every invocation of next() gets traced
-    final Iterator<String> tracedInput =
-        new Iterator<String>() {
-          @Override
-          public boolean hasNext() {
-            return input.hasNext();
-          }
+  private final WindowMaker sut;
 
-          @Override
-          public String next() {
-            final String value = input.next();
-            trace.add(new InputEvent<>(value));
-            return value;
-          }
-        };
-    // create an output sink so that every invocation of update() gets traced
+  /** The actual interaction event trace. */
+  private final List<TraceEvent> trace = new LinkedList<>();
+
+  public Tracing(final WindowMaker sut) {
+    this.sut = sut;
+  }
+
+  public List<TraceEvent> run(final Iterator<String> input) {
+    final Iterator<String> tracedInput = traced(input);
+    // output sink that traces every invocation of update()
     final Output outputToTrace =
         (final Queue<String> value) -> {
           final List<String> copyOfValue = new LinkedList<>(value);
-          trace.add(new OutputEvent<>(copyOfValue));
+          trace.add(new OutputEvent(copyOfValue.toArray(new String[] {})));
         };
-    // the expected trace
-    final List<TraceEvent<String, List<String>>> expectedTrace = new LinkedList<>();
-    expectedTrace.add(new InputEvent<>("asdf"));
-    expectedTrace.add(new OutputEvent<>(Arrays.asList("asdf")));
-    expectedTrace.add(new InputEvent<>("qwer"));
-    expectedTrace.add(new OutputEvent<>(Arrays.asList("asdf", "qwer")));
-    expectedTrace.add(new InputEvent<>("oiui"));
-    expectedTrace.add(new OutputEvent<>(Arrays.asList("asdf", "qwer", "oiui")));
-    expectedTrace.add(new InputEvent<>("zxcv"));
-    expectedTrace.add(new OutputEvent<>(Arrays.asList("qwer", "oiui", "zxcv")));
-    // exercise the SUT
     sut.slidingWindow(tracedInput, outputToTrace);
-    // make sure the expected and actual traces match
-    assertEquals(expectedTrace, trace);
+    return Collections.unmodifiableList(trace);
+  }
+
+  /** Instruments (decorates) the iterator to trace every invocation of next(). */
+  private Iterator<String> traced(final Iterator<String> input) {
+    return new Iterator<String>() {
+      @Override
+      public boolean hasNext() {
+        return input.hasNext();
+      }
+
+      @Override
+      public String next() {
+        final String value = input.next();
+        trace.add(new InputEvent(value));
+        return value;
+      }
+    };
   }
 }
